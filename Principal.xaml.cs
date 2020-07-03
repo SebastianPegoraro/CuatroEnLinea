33e156603c86e3c8f556cf6ccf8e3184839e3a30
adrianvillalba1993 <53920195+adrianvillalba1993@users.noreply.github.com>
2020-07-03 16:15:30 -0300

    retoques finales
    -cambiado  el titulo de la pantalla principal
    -agregando musica de fondo al juego.

--- Principal.xaml.cs
+++ Principal.xaml.cs
@@ -9,6 +9,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 
@@ -17,10 +18,12 @@ namespace TPfinal
     
     public partial class Principal : Window
     {
-        
+        SoundPlayer comienzo = new SoundPlayer("comienzo.wav");
         public Principal()
         {
             InitializeComponent();
+            WindowStartupLocation = System.Windows.WindowStartupLocation.CenterScreen;          
+            comienzo.Play();
         }
 
         private void btnAI_Click(object sender, RoutedEventArgs e)
@@ -28,6 +31,7 @@ namespace TPfinal
             MainWindow juegoAI = new MainWindow();
             juegoAI.VsAI = true;
             this.Close();
+            
             juegoAI.Show();
         }
 
